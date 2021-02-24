59f484d5cb55dcf911b1f02a1b76bee7229f9b02
Thomas Knickman <thomask@surveymonkey.com>
2021-02-24 14:46:15 -0500

    Updated tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,32 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders header", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const header = screen.getByText(/Welcome!/i);
+  expect(header).toBeInTheDocument();
+});
+
+test("renders footer", () => {
+  render(<App />);
+  const footer = screen.getByText("Copyright 2021");
+  expect(footer).toBeInTheDocument();
+});
+
+test("renders first content block", () => {
+  render(<App />);
+  const footer = screen.getByText("Yar Pirate Ipsum");
+  expect(footer).toBeInTheDocument();
+});
+
+test("renders second content block", () => {
+  render(<App />);
+  const footer = screen.getByText("Cat Ipsum");
+  expect(footer).toBeInTheDocument();
+});
+
+test("renders third content block", () => {
+  render(<App />);
+  const footer = screen.getByText("Lit Ipsum");
+  expect(footer).toBeInTheDocument();
 });
